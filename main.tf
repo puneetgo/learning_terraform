3f421d381a1e078109fa771d810b2b8bf092e85b
puneetgo <53733751+puneetgo@users.noreply.github.com>
2024-01-03 17:33:38 +0530

    new sg

--- main.tf
+++ main.tf
@@ -14,6 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "new_SG"
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
